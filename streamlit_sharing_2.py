e0f7d730416eaf543962b2ee75c365c5e6839385
ryand <ryandominguez@protonmail.com>
2022-05-16 01:11:10 -0500

    .

--- streamlit_sharing_2.py
+++ streamlit_sharing_2.py
@@ -18,12 +18,8 @@ import streamlit as st
 import pandas as pd
 from gsheetsdb import connect
 
-st.title("My First Streamlit Web App")
 
-df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
-st.write(df)
-
-st.title("Connect to Google Sheets")
+st.title("KJV Bible")
 gsheet_url = "https://docs.google.com/spreadsheets/d/1pjYzEl-Wlr2X40ECZwesX0_paxLTyssNKFjm1rHbR0U/edit?usp=sharing"
 conn = connect()
 rows = conn.execute(f'SELECT * FROM "{gsheet_url}"')
