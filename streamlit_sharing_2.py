5ca715d7cdd4fd46b5b765e498f3afdb5cdc7845
ryand <ryandominguez@protonmail.com>
2022-05-17 00:28:35 -0500

    .

--- streamlit_sharing_2.py
+++ streamlit_sharing_2.py
@@ -27,7 +27,7 @@ books = df['book'].unique()
 book_choice = st.sidebar.multiselect('Book:', books)
 chapter = df["chapter"].loc[df["book"].isin([book_choice])].unique()
 chapter_choice = st.sidebar.multiselect('Chapter', chapter)
-verse_number = df["verse_number"].loc[df["chapter"] == chapter_choice].unique()
+verse_number = df["verse_number"].loc[df["chapter"].isin([chapter_choice])].unique()
 verse_number_choice = st.sidebar.multiselect('Verse', verse_number)
 
 output = df.loc[(df['book']==book_choice) & (df['chapter']==chapter_choice) & (df['verse_number']== verse_number_choice)]
