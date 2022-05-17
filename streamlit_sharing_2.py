6d2b6ba00bca7a10b4f93f1458267e5de9b901ed
ryand <ryandominguez@protonmail.com>
2022-05-17 00:09:43 -0500

    .

--- streamlit_sharing_2.py
+++ streamlit_sharing_2.py
@@ -19,15 +19,17 @@ conn = connect()
 rows = conn.execute(f'SELECT * FROM "{gsheet_url}"')
 df = pd.DataFrame(rows)
 
-books = df['book'].unique()
-chapter = df['chapter']
-verse_number = df['verse_number']
-output = df
+#books = df['book'].unique()
+#chapter = df['chapter']
+#verse_number = df['verse_number']
+#output = df
 
 book_choice = st.sidebar.selectbox('Which book(s)?:', books)
 chapter = df["chapter"].loc[df["book"] == book_choice]
 chapter_choice = st.sidebar.selectbox('', chapter)
 verse_number = df["verse_number"].loc[df["chapter"] == chapter_choice]
 verse_number_choice = st.sidebar.selectbox('', verse_number)
+
+output = df.loc[(df['book']==book_choice) & (df['chapter']==chapter_choice) & (df['verse_number']== verse_number_choice)]
 
 st.write('Results:', output)
