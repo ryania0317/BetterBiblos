4798041828e467ce0c660ca97bf2251c4e19ec2a
ryand <ryandominguez@protonmail.com>
2022-05-17 01:20:47 -0500

    .

--- streamlit_sharing_2.py
+++ streamlit_sharing_2.py
@@ -28,7 +28,7 @@ books=np.insert(books,0,'All')
 book_choice = st.sidebar.multiselect('Book:', books, default='All')
 book_choice = [all_books for all_books in all_books if 'All' in book_choice]
 st.write(book_choice)
-chapter = df["chapter"].loc[df["book"].isin([book_choice])].unique()
+chapter = df["chapter"].loc[df["book"].isin(book_choice)].unique()
 chapter=np.insert(str(chapter),0,'All')
 st.write(chapter)
 #chapter = np.where('All' in any(books for books in book_choice),all_books,df["chapter"].loc[df["book"].isin([book_choice])].unique())
