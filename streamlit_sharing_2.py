6999b2f7f1574c71acdc414a5ddc2209ffdbaed3
ryand <ryandominguez@protonmail.com>
2022-05-18 00:56:26 -0500

    .

--- streamlit_sharing_2.py
+++ streamlit_sharing_2.py
@@ -28,6 +28,8 @@ books = df['book'].unique()
 all_books = books
 books=np.insert(books,0,'All')
 
+st.write(all_books)
+
 book_choice = st.sidebar.multiselect('Book:', books, default='All')
 book_choice = [all_books if "All" in book_choice else book_choice for book_choice in book_choice]
 
