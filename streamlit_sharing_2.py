2b19ac15a015445009b5872aef0768f179a5138b
ryand <ryandominguez@protonmail.com>
2022-05-18 00:17:07 -0500

    .

--- streamlit_sharing_2.py
+++ streamlit_sharing_2.py
@@ -30,7 +30,7 @@ books=np.insert(books,0,'All')
 #
 book_choice = st.sidebar.multiselect('Book:', books, default='All')
 #st.write(book_choice)
-book_choice = [all_books if "All" in book_choice else book_choice for all_books in all_books]
+book_choice = [all_books if "All" in book_choice else book_choice for book_choice in book_choice]
 st.write(book_choice)
 book_choice = (book_choice)
 
