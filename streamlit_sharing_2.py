6bc6ca56719406bb6bdee1714f84fcba5a8052a4
ryand <ryandominguez@protonmail.com>
2022-05-18 00:21:54 -0500

    .

--- streamlit_sharing_2.py
+++ streamlit_sharing_2.py
@@ -46,7 +46,7 @@ verse_number_choice = [verse_number if 'All' in verse_number_choice else verse_n
 #verse_number_choice = [all_verses for all_verses in verse_number if 'All' in verse_number_choice]
 
 
-output = df.loc[(df['book'].isin([book_choice])) & (df['chapter'].isin([chapter_choice])) & (df['verse_number'].isin([verse_number_choice]))]
+output = df.loc[(df['book'].isin(book_choice)) & (df['chapter'].isin(chapter_choice)) & (df['verse_number'].isin(verse_number_choice))]
 
 
 st.write(type(book_choice))
