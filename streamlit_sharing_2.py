45f9993675fe4dca325b9f5dde974d44090756c9
ryand <ryandominguez@protonmail.com>
2022-05-22 22:04:23 -0500

    .

--- streamlit_sharing_2.py
+++ streamlit_sharing_2.py
@@ -36,14 +36,14 @@ chapter = df["chapter"].unique()
 chapter_all=np.insert(chapter.astype(str),0,'All')
 chapter_choice = st.sidebar.multiselect('Chapter', chapter_all, default='All')
 chapter_choice = [chapter if "All" in chapter_choice else chapter_choice for chapter_choice in chapter_choice]
-chapter_choice = [item for sublist in chapter_choice for item in sublist]
+chapter_choice = [item if len(chapter_choice) > 1 else sublist for sublist in chapter_choice for item in sublist]
 
 verse_number = df["verse_number"].loc[df["chapter"].isin([chapter_choice]) & df["book"].isin([book_choice])].unique()
 verse_number = df["verse_number"].unique()
 verse_number_all=np.insert(verse_number.astype(str),0,'All')
 verse_number_choice = st.sidebar.multiselect('Verse', verse_number_all, default='All')
 verse_number_choice = [verse_number if "All" in verse_number_choice else verse_number_choice for verse_number_choice in verse_number_choice]
-verse_number_choice = [item for sublist in verse_number_choice for item in sublist]
+verse_number_choice = [item if len(verse_number_choice) > 1 else sublist for sublist in verse_number_choice for item in sublist]
 
 book_filter = df['book'].isin(book_choice)
 #chapter_filter = df['chapter'].isin([np.vectorize(np.int(item)) for item in list(chapter_choice)])
@@ -54,13 +54,13 @@ verse_filter = df['verse_number'].isin([item.astype(int) for item in verse_numbe
 output = df.loc[book_filter & chapter_filter & verse_filter]
 
 
-st.write(book_choice)
-st.write(book_filter)
-st.write(chapter_choice)
-st.write(chapter_filter)
-st.write(verse_number_choice)
-st.write(verse_filter)
+#st.write(book_choice)
+#st.write(book_filter)
+#st.write(chapter_choice)
+#st.write(chapter_filter)
+#st.write(verse_number_choice)
+#st.write(verse_filter)
 
 st.write('Results:', output)
 
-st.write(df)
+#st.write(df)
